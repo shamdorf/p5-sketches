3ea27b18d36ff69b0c98c55717a189b5b4be4aaa
shamdorf <41855109+shamdorf@users.noreply.github.com>
2025-07-11 21:19:57 +0200

    Add files via upload

---
+++ sketches.js
@@ -0,0 +1,203 @@
+function autoResizeCanvas(p) {
+  setTimeout(() => {
+    const canvas = p.canvas;
+    if (canvas) {
+      const width = canvas.offsetWidth || p.width;
+      const height = canvas.offsetHeight || p.height;
+
+      window.parent.postMessage(
+        {
+          type: "resize-iframe",
+          width,
+          height,
+        },
+        "*"
+      );
+    }
+  }, 50); // Verzögerung, damit Canvas wirklich da ist
+}
+
+function sketchPoint(p) {
+  p.setup = () => {
+    p.createCanvas(100, 100);
+    p.strokeWeight(5);
+    p.point(50, 50);
+
+    autoResizeCanvas(p);
+  };
+}
+
+function sketchLine(p) {
+  p.setup = () => {
+    p.createCanvas(100, 100);
+    p.line(20, 80, 80, 20);
+    p.strokeWeight(5);
+    p.point(20, 80);
+    p.point(80, 20);
+
+    autoResizeCanvas(p);
+  };
+}
+
+function sketchTriangle(p) {
+  p.setup = () => {
+    p.createCanvas(100, 100);
+    p.triangle(10, 90, 50, 10, 90, 90);
+    p.strokeWeight(5);
+    p.point(10, 90);
+    p.point(50, 10);
+    p.point(90, 90);
+
+    autoResizeCanvas(p);
+  };
+}
+
+function sketchRect(p) {
+  p.setup = () => {
+    p.createCanvas(100, 100);
+    p.rect(20, 30, 60, 40);
+    p.strokeWeight(5);
+    p.point(20, 30);
+
+    autoResizeCanvas(p);
+  };
+}
+
+function sketchRectMode(p) {
+  p.setup = () => {
+    p.createCanvas(500, 400);
+    p.textSize(10);
+    p.rectMode(p.CORNER);
+    p.rect(20, 20, 20, 60);
+    p.text("CORNER, Linke obere Ecke 20, 20 + 20 Breite und 60 Höhe", 10, 15);
+
+    p.rectMode(p.CENTER);
+    p.rect(150, 80, 60, 20);
+    p.text("CENTER, Mittelpunkt 150, 80 + 60 Breite und 20 Höhe", 150, 60);
+
+    p.rectMode(p.CORNERS);
+    p.rect(100, 200, 200, 300);
+    p.text("CORNERS, Linke obere Ecke 100, 200", 80, 190);
+    p.text("CORNERS, Rechte untere Ecke 200, 300", 210, 310);
+
+    p.strokeWeight(5);
+    p.point(20, 20);
+    p.point(150, 80);
+    p.point(100, 200);
+    p.point(200, 300);
+
+    autoResizeCanvas(p);
+  };
+}
+
+function sketchEllipse(p) {
+  p.setup = () => {
+    window.parent.postMessage(
+      {
+        type: "resize-iframe",
+        width: p.width,
+        height: p.height,
+      },
+      "*"
+    );
+    p.createCanvas(100, 100);
+    p.ellipse(50, 50, 60, 30);
+    p.strokeWeight(5);
+    p.point(50, 50);
+  };
+}
+
+function sketchEllipseMode(p) {
+  p.setup = () => {
+    p.createCanvas(500, 350);
+    p.textSize(10);
+    p.ellipseMode(p.CENTER);
+    p.ellipse(50, 50, 20, 60);
+    p.text("CENTER, Mittelpunkt 50, 50 + 20 Breite und 60 Höhe", 10, 15);
+
+    p.ellipseMode(p.CORNER);
+    p.ellipse(150, 80, 60, 20);
+    p.text("CORNER, Linke obere Ecke 150, 80 + 60 Breite und 20 Höhe", 150, 70);
+
+    p.ellipseMode(p.CORNERS);
+    p.ellipse(100, 200, 200, 300);
+    p.text("CORNERS, Linke obere Ecke 100, 200", 80, 190);
+    p.text("CORNERS, Rechte untere Ecke 200, 300", 210, 310);
+
+    p.noFill();
+    p.rect(150, 80, 60, 20);
+    p.rect(100, 200, 100, 100);
+
+    p.strokeWeight(5);
+    p.point(50, 50);
+    p.point(150, 80);
+    p.point(100, 200);
+    p.point(200, 300);
+
+    autoResizeCanvas(p);
+  };
+}
+
+function sketchCircle(p) {
+  p.setup = () => {
+    p.createCanvas(100, 100);
+    p.circle(50, 50, 40);
+    p.strokeWeight(5);
+    p.point(50, 50);
+
+    autoResizeCanvas(p);
+  };
+}
+
+function sketchArc(p) {
+  p.setup = () => {
+    p.createCanvas(220, 180);
+    p.background(255);
+    p.stroke(0);
+    p.fill(200, 100, 100);
+    p.arc(100, 100, 80, 80, p.radians(0), p.radians(90));
+    p.fill(0);
+    p.textSize(10);
+    p.text("0°", 155, 100);
+    p.text("90°", 100, 165);
+    p.text("Uhrzeigersinn", 130, 20);
+
+    p.noFill();
+    p.stroke(150);
+    p.circle(100, 100, 80);
+    p.stroke(0);
+    p.line(100, 100, 140, 100);
+    p.line(100, 100, 100, 140);
+
+    autoResizeCanvas(p);
+  };
+}
+
+function sketchBeginShape(p) {
+  p.setup = () => {
+    p.createCanvas(300, 350);
+
+    p.beginShape();
+    p.vertex(50, 10);
+    p.vertex(80, 80);
+    p.vertex(5, 35);
+    p.vertex(10, 0);
+    p.vertex(30, 30);
+    p.endShape(p.CLOSE);
+
+    p.noFill();
+    p.beginShape();
+    p.vertex(100, 300);
+    p.vertex(100, 100);
+    p.vertex(200, 300);
+    p.vertex(100, 300);
+    p.vertex(200, 100);
+    p.vertex(100, 100);
+    p.vertex(150, 50);
+    p.vertex(200, 100);
+    p.vertex(200, 300);
+    p.endShape();
+
+    autoResizeCanvas(p);
+  };
+}
