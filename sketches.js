f569e49dc2bfcf20c1aa92422d14c316bf79d3ee
shamdorf <41855109+shamdorf@users.noreply.github.com>
2025-08-17 11:26:21 +0200

    !

--- sketches.js
+++ sketches.js
@@ -1159,7 +1159,7 @@ function sketchBilder(p) {
 
   p.draw = () => {
     p.background(255);
-    p.image(img, 0, 0, p, width, p.height);
+    p.image(img, 0, 0, p, p.width, p.height);
     autoResizeCanvas(p); // Anpassung für das iframe
   };
 }
